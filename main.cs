19678e02cb2f4de2e98ff032c80f344017524041
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-31 00:10:14 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+// Created by: Joanne Santhosh
+// Created on: May 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+      int number;
+
+      // input
+      Console.WriteLine("Please enter a positive or negative number:");
+      number = int.Parse(Console.ReadLine());
+
+      // process
+      if (number < 0) {
+      Console.WriteLine("The number is negative!");
+      } else {
+      Console.WriteLine("The number is positive!");
+      }
+      Console.WriteLine("Done.");
+    }
+}     
