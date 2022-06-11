dc6a35ce858c268835b1db67dfd3d3ae8e6f67bb
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-11 20:02:28 +0000

    fix spacing

--- main.cs
+++ main.cs
@@ -10,17 +10,15 @@ class Program
     public static void Main(string[] args)
     {
         int number;
-      
         // input
         Console.WriteLine("Please enter a positive or negative number:");
         number = int.Parse(Console.ReadLine());
 
         // process
-        if (number < 0)  {
-        Console.WriteLine("The number is negative!");
-        } else  {
-        Console.WriteLine("The number is positive!");
-        }
+        if (number < 0)  
+           Console.WriteLine("The number is negative!");
+        else  
+           Console.WriteLine("The number is positive!");
         Console.WriteLine("Done.");
     }
 }    
